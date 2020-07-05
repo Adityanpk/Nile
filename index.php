affc44e814551dacf8aafe4b0f912af13e8a8c0e
Adityanpk <61836096+Adityanpk@users.noreply.github.com>
2020-07-05 22:52:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,48 @@
             <button class="n-button n-rounded n-blue-hover"><i class="fa fa-user"></i>Login</button>
         </div>
     </div>
+<!--*sliding background-->
+<script src="js/slideimg.js"></script>
+<div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <img src="slideimg/img1.jpg" style="width:100%">
+    <div class="text">Caption Text</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img src="slideimg/img2.jpg" style="width:100%">
+    <div class="text">Caption Two</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="slideimg/img3.jpg" style="width:100%">
+    <div class="text">Caption Three</div>
+  </div>
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img src="slideimg/img4.jpg" style="width:100%">
+    <div class="text">Caption Two</div>
+
+  </div>  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img src="slideimg/img5.jpg" style="width:100%">
+    <div class="text">Caption Two</div>
+  </div>
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+  <span class="dot" onclick="currentSlide(4)"></span>
+  <span class="dot" onclick="currentSlide(5)"></span>
+</div>
+<br>
 
     <div>   
       <div class="n-logoimage">
